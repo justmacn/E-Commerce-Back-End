07a829f86c5cdacde9f0a30e9c5148379ffdc4e6
justmacn <172618812+justmacn@users.noreply.github.com>
2024-08-05 20:57:38 -0400

    added get routes for tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,18 +3,44 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
+
+// find all tags
+router.get('/', async (req, res) => {
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product, thorugh: ProductTag }],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
+
+
+// find a single tag by its `id`
+router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, thorugh: ProductTag }],
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+
+// create a new tag
 router.post('/', (req, res) => {
-  // create a new tag
 });
 
 router.put('/:id', (req, res) => {
